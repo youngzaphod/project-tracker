b03c7b21ff75a4906a633fe64a67453236770956
Young Zaphod <johndurso@gmail.com>
2019-02-15 23:36:44 -0800

    quick fix removed unused path

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
