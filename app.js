55f336924952db267dae0b2cb5cb341ebd454aa3
Young Zaphod <johndurso@gmail.com>
2019-02-15 23:29:36 -0800

    updated app.js from default to include conditions for production

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+const indexRouter = require('./routes/index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Production mode path
+if (process.env.NODE_ENV === 'production') {
+    console.log("Production mode");
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    
+} else {
+    // Build mode path
+    console.log("Dev mode");
+    app.use(express.static(path.join(__dirname, 'client/public')));
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
