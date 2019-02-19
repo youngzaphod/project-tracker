83301a0cbe6c6fb739fe0ac14c0a56c790692203
andrewmayer <andrew@levelupdesign.com>
2019-02-18 19:14:34 -0800

    Starting the Database editor

--- data-structure.js
+++ data-structure.js
@@ -1,41 +1,72 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/protrack_app', { useNewUrlParser: true });
+//Connect to mongodb "protrack-app"
+mongoose.connect('mongodb://localhost:27017/protrack-app', { useNewUrlParser: true });
 
 const protrackSchema = new mongoose.Schema({
-	Name: String,
-	duedate: Date, //Definitely need to understand more about date objects.
-	tasks: { //This nested object structure still needs to be tested
-		name:	String,
-		taskdate: Number,
-		Status:	String,
-		Description: String
-	}
+	name: String,
+	duedate: String,
+	status: String,
+
 })
 
-const ProTrack = mongoose.model("Milestone", protrackSchema);
+// This is creating (or opening) the ProTrack schema that has all the methods attached.
+// Collection name will be Milestones
+const Milestone = mongoose.model("Milestone", protrackSchema);
 
-// ? Somewhere this is creating the 
+// let writeme = new Milestone({
+// 	name: "Test2",
+// 	duedate: "02/25/2019",
+// 	status: "overdue",
+// 	tasks: {
+// 		name: "Call Harry",
+// 		taskdate: "03/01/2019",
+// 		status: "on time",
+// 		order: 3,
+// 		description: 'testing out the list'
+// 	}
+// })
+
+// writeme.save((err, Milestone) => {
+// 	if (err) {
+// 		console.log(`Something went wrong`)
+// 	} else {
+// 		console.log(`Test is saved to the db`);
+// 		console.log(Milestone);
+// 	}
+// });
+
+//retrieve all records from model.
+
+// Milestone.find({}, (err, milestones) => {
+// 	if(err) {
+// 		console.log(`An ERROR has occured`);
+// 		console.log(err)
+// 	} else {
+// 		console.log("")
+// 		console.log(milestones);
+// 	}
+// });
 
 Milestone.create({
-   Name: "Test",
-   duedate: ISODate("2019-03-01T01:01:01.001Z"),
-   temperament: "Sweet"
-}, (err, cat) => {
-    if(err){
-        console.log(err);
-    } else {
-        console.log(cat);
-    }
+	name: "Test4",
+ 	duedate: "03/25/2019",
+ 	status: "on-time",
+}, (err,newMStone) => {
+	if (err) {
+		console.log(`An ERROR has occured`);
+	} else {
+		console.log(newMStone);
+	}
 });
 
-//retrieve all cats from the DB and console.log them.
+// //retrieve all cats from the DB and console.log them.
 
-Tasklist.find({}, (err, cats) => {
-    if(err) {
-        console.log("ERROR ERROR!!!");
-        console.log(err);
-    } else {
-        console.log("See all my cats and despair:");
-        console.log(cats);
-    }
-})
+// Tasklist.find({}, (err, Milestones) => {
+//     if(err) {
+//         console.log("ERROR ERROR!!!");
+//         console.log(err);
+//     } else {
+//         console.log("See all my Milestones and despair:");
+//         console.log(cats);
+//     }
+// })
