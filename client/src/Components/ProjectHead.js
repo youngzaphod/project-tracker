955cbbd88937e1351cfe02f67f7870dbeb67f689
andrewmayer <andrew@levelupdesign.com>
2019-03-21 10:04:50 -0700

    created ProjectHead component

---
+++ client/src/Components/ProjectHead.js
@@ -0,0 +1,101 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "react-datepicker/dist/react-datepicker.css";
+import Button from "react-bootstrap/Button";
+import { FaCog } from "react-icons/fa";
+import DatePicker from "react-datepicker";
+
+class ProjectHead extends React.Component {
+  state = {
+    name: "",
+    start: "",
+    finish: "",
+    startErrorMsg: "",
+    finishErrorMsg: "",
+    errMessage: ""
+  };
+
+  handleName = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleDayChange = (date, which) => {
+    let errorSet = "";
+    console.log(date);
+    if (
+      (which === "start" &&
+        this.state.finish !== "" &&
+        date > this.state.finish) ||
+      (which === "finish" && this.state.start !== "" && date < this.state.start)
+    ) {
+      errorSet = "Start date must come before finish date!";
+    }
+    this.setState({
+      [which]: date,
+      errMessage: errorSet
+    });
+  };
+
+  render() {
+    return (
+      <Row noGutters className="justify-content-center">
+        <Col lg={4} md={5} sm={10}>
+          <br />
+          <Form.Control
+            onChange={this.handleName}
+            name="name"
+            size="lg"
+            type="text"
+            placeholder="Project name"
+          />
+        </Col>
+        <Col md={2} sm={5} xs={12}>
+          <br />
+          <DatePicker
+            maxDate={this.state.finish !== "" ? this.state.finish : null}
+            placeholderText="Start"
+            showMonthDropdown
+            showYearDropdown
+            customInput={
+              <Form.Control
+                onClick={this.props.onClick}
+                size="lg"
+                value={this.props.value}
+              />
+            }
+            onChange={date => this.handleDayChange(date, "start")}
+            selected={this.state.start !== "" ? this.state.start : null}
+          />
+        </Col>
+        <Col lg={2} md={2} sm={5} xs={12}>
+          <br />
+          <DatePicker
+            minDate={this.state.start !== "" ? this.state.start : null}
+            placeholderText="Finish"
+            customInput={
+              <Form.Control
+                onClick={this.props.onClick}
+                size="lg"
+                value={this.props.value}
+              />
+            }
+            onChange={date => this.handleDayChange(date, "finish")}
+            selected={this.state.finish !== "" ? this.state.finish : null}
+          />
+        </Col>
+        <Col sm={1}>
+          <br />
+          <Button variant="light">
+            <FaCog size={26} />
+          </Button>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default ProjectHead;
--- client/src/Components/Task.js
+++ client/src/Components/Task.js
@@ -1,77 +1,89 @@
-import React, { Component } from 'react';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-import { FaArrowDown, FaArrowUp, FaTrashAlt, FaArrowsAltV } from 'react-icons/fa';
-import { MdAdd } from 'react-icons/md';
-import { SortableHandle } from 'react-sortable-hoc';
+import React, { Component } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import {
+  FaArrowDown,
+  FaArrowUp,
+  FaTrashAlt,
+  FaArrowsAltV
+} from "react-icons/fa";
+import { MdAdd } from "react-icons/md";
+import { SortableHandle } from "react-sortable-hoc";
 
-const DragHandle = SortableHandle(() => <span> <FaArrowsAltV /></span>);
+const DragHandle = SortableHandle(() => (
+  <span>
+    {" "}
+    <FaArrowsAltV />
+  </span>
+));
 
 class Task extends Component {
-    state = {
-        name: this.props.name,
-        hover: 'hidden',
-    };
+  state = {
+    name: this.props.name,
+    hover: "hidden"
+  };
 
-    onNameChange = (e) => {
-        this.props.onType(e.target.value);
-    }
+  onNameChange = e => {
+    this.props.onType(e.target.value);
+  };
 
-    render() {
-        return (
-            <div className='milestone highlight-fade'
-                onMouseEnter={() => this.setState({hover: 'visible'})}
-                onMouseLeave={() => this.setState({hover: 'hidden'})}
-            >
-            <Row>
-            <Col md={8}>
+  render() {
+    return (
+      <div
+        className="milestone highlight-fade"
+        onMouseEnter={() => this.setState({ hover: "visible" })}
+        onMouseLeave={() => this.setState({ hover: "hidden" })}
+      >
+        <Row>
+          <Col md={8}>
             <OverlayTrigger
-                placement='top'
-                overlay={
-                    <Tooltip id='tooltip-top'>
-                        Add task above
-                    </Tooltip>
-                }
+              placement="top"
+              overlay={<Tooltip id="tooltip-top">Add task above</Tooltip>}
             >
-                <button
-                    style={{visibility: this.state.hover}}
-                    onClick={this.props.addAbove}
-                >
-                    <MdAdd />
-                </button>
+              <button
+                style={{ visibility: this.state.hover }}
+                onClick={this.props.addAbove}
+              >
+                <MdAdd />
+              </button>
             </OverlayTrigger>
-            <span style={{visibility: this.state.hover}}><DragHandle /></span>
+            <span style={{ visibility: this.state.hover }}>
+              <DragHandle />
+            </span>
             <input
-                id={this.props.id}
-                name='milestone'
-                type='text'
-                className='milestone-input'
-                placeholder='New task'
-                defaultValue={this.props.name}
-                onChange={this.onNameChange}
+              id={this.props.id}
+              name="milestone"
+              type="text"
+              className="milestone-input"
+              placeholder="New task"
+              defaultValue={this.props.name}
+              onChange={this.onNameChange}
             />
-            </Col>
-            <Col md={2}>
+          </Col>
+          <Col md={2}>
             <input
-                id={this.props.id}
-                name='units'
-                type='text'
-                className='units-input'
-                placeholder='Units'
-                defaultValue={this.props.units}
+              id={this.props.id}
+              name="units"
+              type="text"
+              className="units-input"
+              placeholder="Units"
+              defaultValue={this.props.units}
             />
-            
-            </Col>
-            <Col md={1}>
-                <button style={{visibility: this.state.hover}} onClick={this.props.deleteItem}><FaTrashAlt /></button>
-            </Col>
-            </Row>
-            </div>
-        )
-    }
-
+          </Col>
+          <Col md={1}>
+            <button
+              style={{ visibility: this.state.hover }}
+              onClick={this.props.deleteItem}
+            >
+              <FaTrashAlt />
+            </button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Task;
